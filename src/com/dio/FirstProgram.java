c61bca14f58a6c5f0de9cef93036d8461257f515
souza-pablo <pablo.souza.5@outlook.com.br>
2022-03-04 03:21:54 -0300

    first commit

---
+++ src/com/dio/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World!");
+	}
+
+}
